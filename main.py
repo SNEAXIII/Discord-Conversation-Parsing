e3e1467b1e680f7554d46d75526dfe54d980253f
AlexisB <misterbalise2@gmail.com>
2023-10-09 00:08:05 +0200

    modify main for test purpose

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from src.AllMessages import AllMessages
 from pprint import pprint
 
 test = AllMessages(["MisterBalise", "Gibus"])
-
-pprint(test.listMessages[0])
+premierMessage = test.listMessages[20]
+print(premierMessage)
+# print(premierMessage.author.name)
