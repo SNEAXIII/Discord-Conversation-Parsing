4159608c72882bc88bc8c8daaeb267557408623d
AlexisB <misterbalise2@gmail.com>
2023-10-08 21:36:48 +0200

    ajout AllMessages avec un fix a apporter sur le sort

---
+++ src/AllMessages.py
@@ -0,0 +1,23 @@
+from os import path
+import message
+from random import randint
+
+
+class AllMessages:
+    def __init__(self, listMember):
+        self.listMember = listMember
+        self.listMessages = self.getAllMessagesFromCSV(self.listMember)
+        # sorted(liste, key=lambda x: x[0])
+        pass
+
+    def getAllMessagesFromCSV(self, listMember):
+        listMessages = []
+        for member in self.listMember:
+            csvPath = path.join("data", "csv", member)
+            with open(csvPath, "r", encoding="utf-8") as csvfile:
+                reader = reader(csvfile)
+                next(reader)
+                for row in reader:
+                    message(row)
+        # À fixer, car ce ne sera pas une liste, mais une liste d'ojet
+        return sorted(listMessages, key=lambda x: x[0])
