679caf2f9354e366eec8eda9185e8accba85d425
AlexisB <misterbalise2@gmail.com>
2023-10-11 00:17:25 +0200

    try to add multi line text + date in gray

--- tests/imageBuilder.py
+++ tests/imageBuilder.py
@@ -2,15 +2,33 @@ from PIL import Image, ImageDraw, ImageFont
 
 image = Image.new("RGB", (400, 200), (49, 51, 56))
 draw = ImageDraw.Draw(image)
-font1 = ImageFont.truetype("font/ggsans-Bold.ttf", 14)
-font2 = ImageFont.truetype("font/ggsans-Medium.ttf", 14)
+font1 = ImageFont.truetype("../font/ggsans-Bold.ttf", 14)
+font2 = ImageFont.truetype("../font/ggsans-Medium.ttf", 14)
+font3 = ImageFont.truetype("../font/ggsans-Medium.ttf", 12)
+red = (255, 0, 0)  # Rouge
+
 x = 50
 y = 60
 text = "Gibus"
 draw.text((x, y), text, font=font1, fill="white")
+
+# Ajouter la chaîne "01/06/2019 13:15" à côté de la chaîne "Gibus"
+bbox = draw.textbbox((0, 0), text, font=font1)
+print(bbox)
+x2 = bbox[2] + x + 10  # Ajouter un espace de 10 pixels entre les deux chaînes de texte
+y2 = y+2
+gray = (187, 187, 187)
+text2 = "01/06/2019 13:15"
+draw.text((x2, y2), text2, font=font3, fill=gray)
+
+image.putpixel((x, y), red)
+
 x = 50
 y = 80
-text = "du coup tu m'envoies les panorama?"
-draw.text((x, y), text, font=font2, fill="white")
+text = "du coup tu m'envoies les panorama?\ntest saut de ligne"
+draw.text((x, y), text, font=font2, fill="white", spacing=8)
+
+image.putpixel((x, y), red)
+
 image.save("image_text.png")
 image.show()
