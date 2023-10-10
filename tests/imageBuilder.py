afec6834caf6c3d414ef24fbf8e550001f8fe3f7
AlexisB <misterbalise2@gmail.com>
2023-10-11 00:24:48 +0200

    remoove commentary and fix gray a bit lighter

--- tests/imageBuilder.py
+++ tests/imageBuilder.py
@@ -4,7 +4,7 @@ image = Image.new("RGB", (400, 200), (49, 51, 56))
 draw = ImageDraw.Draw(image)
 font1 = ImageFont.truetype("../font/ggsans-Bold.ttf", 14)
 font2 = ImageFont.truetype("../font/ggsans-Medium.ttf", 14)
-font3 = ImageFont.truetype("../font/ggsans-Medium.ttf", 12)
+font3 = ImageFont.truetype("../font/ggsans-Medium.ttf", 10)
 red = (255, 0, 0)  # Rouge
 
 x = 50
@@ -12,12 +12,11 @@ y = 60
 text = "Gibus"
 draw.text((x, y), text, font=font1, fill="white")
 
-# Ajouter la chaîne "01/06/2019 13:15" à côté de la chaîne "Gibus"
 bbox = draw.textbbox((0, 0), text, font=font1)
 print(bbox)
-x2 = bbox[2] + x + 10  # Ajouter un espace de 10 pixels entre les deux chaînes de texte
-y2 = y+2
-gray = (187, 187, 187)
+x2 = bbox[2] + x + 10
+y2 = y+4
+gray = (174, 174, 174)
 text2 = "01/06/2019 13:15"
 draw.text((x2, y2), text2, font=font3, fill=gray)
 
