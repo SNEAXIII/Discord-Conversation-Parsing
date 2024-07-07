827411064b895d87c6a6d1c2356f438199045c91
AlexisB <misterbalise2@gmail.com>
2024-07-07 22:24:32 +0200

    Fix image png extension

--- src/Author.py
+++ src/Author.py
@@ -6,7 +6,7 @@ class Author:
     def __init__(self, rawString):
         self.name = rawString
         # TODO impelementer les images de pp au bon format
-        _path = path.join("data", "pp", self.name)+".webp"
+        _path = path.join("data", "pp", self.name)+".png"
         self.profilePicture = Image.open(f"{_path}")
         xBase = 20
 
