4a0b1bac54722d7b3640d9befcc533d63c01c232
AlexisB <misterbalise2@gmail.com>
2024-07-10 00:46:32 +0200

    Add reset + parse function

--- src/AllMessages.py
+++ src/AllMessages.py
@@ -1,32 +1,48 @@
 from csv import reader
 from os import path
 
-from src.Author import Author
+from src.Auteur import Auteur
 from src.Message import Message
 
 
 class AllMessages:
     def __init__(self, listMember):
-        self.listAuthor = self.getAllAuthors(listMember)
-        self.listMessages = self.getAllMessagesFromCSV()
+        self.listAuteur = self.getAllAuteurs(listMember)
+        self.reset()
 
-    def getAllAuthors(self, listMember: list) -> list:
-        listAuthor = []
+    def getAllAuteurs(self, listMember: list) -> list:
+        listAuteur = []
         for member in listMember:
-            listAuthor.append(Author(member))
-        return listAuthor
+            listAuteur.append(Auteur(member))
+        return listAuteur
 
     def getAllMessagesFromCSV(self):
         listMessages = []
-        for member in self.listAuthor:
-            csvPath = path.join(".", "data", "csv", member.name) + ".csv"
-            with open(csvPath, "r", encoding="utf-8") as csvfile:
+        for member in self.listAuteur:
+            csvPath = path.join(".", "data", "csv", member.nom) + ".csv"
+            with open(csvPath, encoding="utf-8") as csvfile:
                 _reader = reader(csvfile)
                 next(_reader)
                 for row in _reader:
                     listMessages.append(Message(row, member))
         return self.sortMessagesByDate(listMessages)
 
+    def parseMessage(self, listMessages: list[Message]) -> list[Message]:
+        nouvelleListe = []
+        dernierAuteur = None
+        for messageActuel in listMessages:
+            auteurActuel = messageActuel.auteur
+            if dernierAuteur == auteurActuel:
+                nouvelleListe[-1].text.rawString += f"\n{messageActuel.text.rawString}"
+            else:
+                nouvelleListe.append(messageActuel)
+            dernierAuteur = auteurActuel
+            # todo ajouter les wrap des mots
+        return nouvelleListe
+
     @staticmethod
     def sortMessagesByDate(listMessages: list) -> list:
         return sorted(listMessages, key=lambda x: x.id)
+
+    def reset(self):
+        self.listMessages = self.getAllMessagesFromCSV()
