1dbee72cd511da8fe53250240fb3c5c738a3b1cf
AlexisB <misterbalise2@gmail.com>
2024-07-10 02:04:57 +0200

    Remove useless things

--- main.py
+++ main.py
@@ -3,11 +3,9 @@ from src.ImageBuilder import ImageBuilder
 
 allMessages = AllMessages(["MisterBalise", "Gibus"])
 messages = allMessages.parseMessage(allMessages.listMessages[0:5])
-auteurs = allMessages.listAuteur
 
 for message in messages:
     print(message)
 
 builder = ImageBuilder()
 builder.build(messages)
-a = 0
